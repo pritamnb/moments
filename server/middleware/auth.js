2249a8904e3f72d8a5df71959be725ac75d8e346
Pritam <preetambhalerao00@gmail.com>
2021-02-22 14:33:15 +0530

    email check exist or not

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,26 +1,29 @@
 const isEmpty = require('../libs/checkLib');
 const jwt = require('jsonwebtoken');
-const winLogger = require("../libs/winstonLib");
+const winLogger = require('../libs/winstonLib');
 
-module.exports = (req,res,next)=>{
-    if(isEmpty(req.headers["x-access-token"])){
-        winLogger.error("No Token Provided");
-        return res.status(404).json({
-            error : true,
-            message : "Auth Token Missing"
-        });
-    }else{
-        try{
-            const token = jwt.verify(req.headers["x-access-token"],process.env.JWTSECRET);
-            req.userId = token.Data.userId;
-            winLogger.info("Valid Token Provided");
-            next();
-        }catch(err){
-            winLogger.error("Invalid/ Expired Token Provided");
-            return res.status(403).json({
-                error : true,
-                message : "Invalid Token Provided"
-            });
-        }
+module.exports = (req, res, next) => {
+  if (isEmpty(req.headers['x-access-token'])) {
+    winLogger.error('No Token Provided');
+    return res.status(404).json({
+      error: true,
+      message: 'Auth Token Missing',
+    });
+  } else {
+    try {
+      const token = jwt.verify(
+        req.headers['x-access-token'],
+        process.env.JWTSECRET
+      );
+      req.userId = token.Data.userId;
+      winLogger.info('Valid Token Provided');
+      next();
+    } catch (err) {
+      winLogger.error('Invalid/ Expired Token Provided');
+      return res.status(403).json({
+        error: true,
+        message: 'Invalid Token Provided',
+      });
     }
-}
+  }
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,56 +1,78 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 
-const { createUser, login} = require('../controllers/userController');
+const {
+  createUser,
+  login,
+  getEmail,
+} = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/create',async (req,res)=>{
-    try{
-        let result = await createUser(req.body);
+router.post('/create', async (req, res) => {
+  try {
+    let result = await createUser(req.body);
 
-        if(result.error){
-            res.status(500).json({
-                error: true,
-                message : result.message
-            });
-        }else{
-            res.status(200).json({
-                error : false,
-                data : result.data
-            });
-        }
-    }catch(error){
-        return res.status(500).json({
-            error : true,
-            message: "Something Went Wrong!"
-        });
+    if (result.error) {
+      res.status(500).json({
+        error: true,
+        message: result.message,
+      });
+    } else {
+      res.status(200).json({
+        error: false,
+        data: result.data,
+      });
     }
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: 'Something Went Wrong!',
+    });
+  }
 });
 
-router.post('/login',async (req,res)=>{
-    try{
-        let result = await login(req.body.email,req.body.password);
-        if(result.error){
-            return res.status(500).json({
-                error : true,
-                message : result.message
-            });
-        }else{
-            return res.status(200).json({
-                error : false,
-                message : result.message,
-                data : result.data
-            }); 
-        }
-    }catch(error){
-        return res.status(500).json({
-            error : true,
-            message : "Something Went Wrong!"
-        });
+router.post('/login', async (req, res) => {
+  try {
+    let result = await login(req.body.email, req.body.password);
+    if (result.error) {
+      return res.status(500).json({
+        error: true,
+        message: result.message,
+      });
+    } else {
+      return res.status(200).json({
+        error: false,
+        message: result.message,
+        data: result.data,
+      });
     }
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: 'Something Went Wrong!',
+    });
+  }
 });
+/**
+ * It will trigger when /user/get-email/:email called
+ * @param {*} req
+ * @param {*} res
+ */
+router.get('/get-email/:email', async (req, res) => {
+  const email = req.params.email;
+  console.log(email);
+  // Calling getEmailFromDB() service function to fetch email from DB
+  getEmail(email, (error, users) => {
+    if (error) {
+      return res.status(404).json({ error: error });
+    }
 
+    return res.status(200).json({
+      status: 'Successfully fetched Emails',
+      users: users,
+    });
+  });
+});
 
 module.exports = router;
-
